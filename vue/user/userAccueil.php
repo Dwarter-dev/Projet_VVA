f5a04e4f9b6ce8fc2c72acb39f0a1ac4c51db98d
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2021-01-21 06:21:30 +0100

    Correctif sur la navbar

--- vue/user/userAccueil.php
+++ vue/user/userAccueil.php
@@ -15,6 +15,15 @@
           <a class="dropdown-item" href="index.php?page=consulterAnimation">Consulter une Animation</a>
         </div>
       </li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Activités
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					<a class="dropdown-item" href="index.php?page=creeActivite">Créer une Activite</a>
+					<a class="dropdown-item" href="index.php?page=consulterActivite">Consulter une Activite</a>
+				</div>
+			</li>
 			<a class="nav-item nav-link" href="index.php?page=deconnexion">Déconnexion</a>
 		</div>
 </nav>
