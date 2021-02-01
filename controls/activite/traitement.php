61ff9e9365107a3811019c1f58123fe7558d3c00
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2021-02-01 16:41:28 +0100

    Correction

--- controls/activite/traitement.php
+++ controls/activite/traitement.php
@@ -16,11 +16,11 @@ if(isset($_POST['envoyer']))
   $sql = "INSERT INTO activite (CODEANIM, CODEETATACT, DATEACT, PRIXACT, HRRDVACT, HRDEBUTACT, HRFINACT, DATEANNULEACT, NOMRESP, PRENOMRESP)
   VALUES ('$CODEANIM','$CODEETATACT','$DATEACT','$PRIXACT','$HRRDVACT','$HRDEBUTACT','$HRFINACT','$DATEANNULEACT','$NOMRESP','$PRENOMRESP')";
   if (mysqli_query($conn, $sql)) {
-        echo "Création du profil dans la base de donnée";
+        echo "Création de l'activité dans la base de donnée";
   }
   else {
         echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
   }
   mysqli_close($conn);
 }
-header('Location: http://vva/index.php?page=consulterActivite');
+header('Location: http://vva/index.php?page=consulterActiviteAdmin');
