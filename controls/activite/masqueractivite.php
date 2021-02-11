5b701bc46e5c8a3b99b2159ed0f5794ba068f150
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2021-02-11 16:00:54 +0100

    Cangement du fichier en masqueractivite

--- controls/activite/masqueractivite.php
+++ controls/activite/masqueractivite.php
@@ -1,10 +1,13 @@
 <?php
 include("connexion.php");
+
 if(isset($_POST['envoyerdelete']))
 {
   $ID = intval($_GET['id']);
+  $DATEANNULEACT = new DateTime();
 
-  $sql = "DELETE FROM activite
+  $sql = "UPDATE activite
+          SET DATEANNULEACT = '" . $DATEANNULEACT->format('Y-m-d') . "'
           WHERE NOACT = $ID";
   if (mysqli_query($conn, $sql)) {
         echo "Suppression de l'activité dans la base de donnée";
@@ -15,9 +18,3 @@ if(isset($_POST['envoyerdelete']))
   mysqli_close($conn);
 }
 header('Location: http://vva/index.php?page=consulterActivite');
-/*  $ID = intval($_GET['id']);
-  $DATEANNULEACT = ($_GET['DATEANNULEACT']);
-
-  $sql = "UPDATE activite
-          SET DATEANNULEACT = '$DATEANNULEACT'
-          WHERE NOACT = $ID";*/
