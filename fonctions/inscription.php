37e85dcdc9e29a72b319d596f67126d83afe8654
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2021-02-27 14:36:41 +0100

    Ajout de la condition de la date

--- fonctions/inscription.php
+++ fonctions/inscription.php
@@ -7,15 +7,16 @@ require_once('lib.php');
  */
 function getInscription() {
     $con = mysqli_connect("localhost","root" ,"", "gacti"); // En faire une fonction à mettre dans un fichier lib.php et le require_once dans ton index
-
+    //var_dump($_SESSION);
     $req =
-    "
+    '
     SELECT *
     FROM inscription
-    ";
+    WHERE USER = "'.$_SESSION['USER'].'"
+    AND inscription.DATEANNULE = "0000-00-00"';
 
     $res = mysqli_query($con, $req);
-    $inscription = []; //Va regrouper toutes les animations
+    $inscription = []; //Va regrouper toutes les inscriptions
 
     while($ligne = mysqli_fetch_assoc($res))
       array_push($inscription, $ligne);
