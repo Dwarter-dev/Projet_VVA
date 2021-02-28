b9e760a9eab116ca1e89bd752bb9189a7a1fd357
Steven Da Costa <60145033+Dwarter-dev@users.noreply.github.com>
2021-02-28 10:50:40 +0100

    modifs navbar, correction de CODEETAT et DATEANNULL

--- vue/activite/creeActivite.php
+++ vue/activite/creeActivite.php
@@ -13,26 +13,69 @@ if (empty($_SESSION))
 <nav class="navbar navbar-expand-lg navbar-dark navbar-light" style="background-color: #398ac7">
   <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
     <ul class="navbar-nav mr-auto">
-		  <a class="nav-item nav-link" href="index.php?page=accueil">Village Vacances Alpes </a>
-		  <a class="nav-item nav-link" href="index.php?page=userProfilAdmin">Profil</a>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Activités
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="index.php?page=consulterActivite">Consulter une Activite</a>
-          <a class="dropdown-item" href="index.php?page=inscription">S'inscrire à une activité</a>
-        </div>
-      </li>
-		<li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Animations
-      </a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="index.php?page=creeAnimation">Créer une Animation</a>
-        <a class="dropdown-item" href="index.php?page=consulterAnimation">Consulter une Animation</a>
-      </div>
-    </li>
+			<a class="nav-item nav-link" href="index.php?page=accueil">Village Vacances Alpes </a>
+			<?php
+			if ($_SESSION["TYPEPROFIL"] === 'EN')
+			{
+				echo '<a class="nav-item nav-link" href="index.php?page=userProfilAdmin">Profil</a>';
+			}
+			else
+			{
+				echo '<a class="nav-item nav-link" href="index.php?page=userProfilUser">Profil</a>';
+			}
+			 ?>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" > Activités </a>
+					<ul class="dropdown-menu">
+						<li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Gestion des activités</a>
+							<ul class="dropdown-menu">
+								<?php
+								if ($_SESSION["TYPEPROFIL"] === 'EN')
+								{
+									echo '<li><a class="dropdown-item" href="index.php?page=consulterActivite">Consulter une Activité</a></li>';
+								}
+								else
+								{
+									echo '';
+								}
+								?>
+							 </ul>
+						</li>
+						<li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Gestion des inscriptions</a>
+							<ul class="dropdown-menu">
+								<?php
+								if ($_SESSION["TYPEPROFIL"] === 'EN')
+								{
+									echo '<li><a class="dropdown-item" href="index.php?page=mesInscriptions">Liste de mes inscriptions</a></li>
+												<li><a class="dropdown-item" href="index.php?page=vueInscription">Liste des inscrits</a></li>';
+								}
+								else
+								{
+									echo '<li><a class="dropdown-item" href="index.php?page=mesInscriptions">Liste de mes inscriptions</a></li>';
+								}
+								?>
+							</ul>
+						</li>
+					</ul>
+		 	</li>
+			<?php
+			if ($_SESSION["TYPEPROFIL"] === 'EN')
+			{
+				echo '<li class="nav-item dropdown">
+					 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						 Animations
+					 </a>
+					 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="index.php?page=creeAnimation">Créer une Animation</a>
+						 <a class="dropdown-item" href="index.php?page=consulterAnimation">Consulter une Animation</a>
+					 </div>
+				 </li>';
+			}
+			else
+			{
+				echo '';
+			}
+			?>
     <a class="nav-item nav-link" href="index.php?page=deconnexion">Déconnexion</a>
 	</div>
 	<div class="mx-auto order-0">
@@ -56,10 +99,7 @@ if (empty($_SESSION))
 		<label for="CodeAnimation" class="col-sm-2 col-form-label col-form-label">Code de l'animation</label>
 		<input name="CODEANIM" id="CODEANIM" type="text" class="form-control">
 	</div>
-  <div class="form-group">
-    <label for="CodeEtatActivite" class="col-sm-2 col-form-label col-form-label">Etat de l'activité</label>
-    <input name="CODEETATACT" id="CODEETATACT" type="text" class="form-control">
-  </div>
+    <input name="CODEETATACT" id="CODEETATACT" type="hidden" class="form-control" placeholder="LB">
   <div class="form-group col-10">
       <label for="dateCreaActivite">Date de l'activité</label>
       <input name="DATEACT" id="DATEACT" type="date" class="form-control" >
@@ -74,16 +114,13 @@ if (empty($_SESSION))
   </div>
 	<div class="form-group col-10">
 			<label for="hrDebutActivite">Heure Début</label>
-			<input name="HRDEBUTACT" id="HRDEBUTACT" type="time" class="form-control" placeholder="ex : 10:00">
+			<input name="HRDEBUTACT" id="HRDEBUTACT" type="time" class="form-control" placeholder="ex : 11:00">
 	</div>
 	<div class="form-group col-10">
 		<label for="hrFinActivite">Heure Fin</label>
-		<input name="HRFINACT" id="HRFINACT" type="time" class="form-control" placeholder="ex : 12:00">
+		<input name="HRFINACT" id="HRFINACT" type="time" class="form-control" placeholder="12:00">
 	</div>
-	<div class="form-group col-10">
-		<label for="dtAnnulActivite">Date d'annulation</label>
-		<input name="DATEANNULEACT" id="DATEANNULEACT" type="date" class="form-control" >
-	</div>
+		<input name="DATEANNULEACT" id="DATEANNULEACT" type="hidden" class="form-control">
 	<div class="form-group">
 		<label for="nomRActivite" class="col-sm-2 col-form-label col-form-label">Nom du Responsable</label>
 		<input name="NOMRESP" id="NOMRESP" type="text" class="form-control">
